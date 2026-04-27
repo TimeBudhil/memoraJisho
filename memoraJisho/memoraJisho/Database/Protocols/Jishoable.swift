584908fc22e80de97f3015c11f752398e97ac8a6
TimeBudhil <71571047+TimeBudhil@users.noreply.github.com>
2026-04-27 10:04:45 -0500

    more functions

--- memoraJisho/memoraJisho/Database/Protocols/Jishoable.swift
+++ memoraJisho/memoraJisho/Database/Protocols/Jishoable.swift
@@ -6,26 +6,28 @@
 //
 
 //functions for dealing with read only SQL database
+//Repository layer, only dealing with queries,
 protocol Jishoable {
     //Main Glossary Entries
     
     /**
      This Function obtains the full entry.
      */
-    func getEntry(unnormalizedInput: String) -> EntryDetail
+    func getEntry(normalizedinput: String, type: inputType) -> EntryDetail
     func getEntryEnglish(english: String) -> EntryDetail
     func getEntryKanji(kanji : String) -> EntryDetail
     func getEntryKana(normalizedKana : String) -> EntryDetail
     func getEntryRomanji(romanji : String) -> EntryDetail
     
     //Name entries
-    func getName(unnormalizedInput: String) -> NameEntry
+    func getName(normalizedinput: String, type: inputType) -> NameEntry
     func getNameKanji(kanji : String) -> NameEntry
     func getNameNanori(nanori : String) -> NameEntry
     func getNameKana(kana : String) -> NameEntry
     
     //radicals
-    func getKanji(unnormalizedInput : String) -> KanjiDetail
+    func getKanji(normalizedinput : String, type: inputType) -> KanjiDetail
+    func getKanjiLiteral(kanjiLiteral : String) -> KanjiDetail
     func getKanjiRadicals(radicals : String) -> KanjiDetail
     func getKanjiReading(reading : String) -> KanjiDetail
 }
